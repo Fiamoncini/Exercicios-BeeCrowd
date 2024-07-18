ed01ef3d5138cf0a3a4ca6d0cb3a774a53d5290f
Fiamoncini <93059776+Fiamoncini@users.noreply.github.com>
2024-07-18 16:18:44 -0300

    Create Ex1001.java

---
+++ Ex1001.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        
+        Scanner soma = new Scanner(System.in);
+        int a = soma.nextInt();
+        int b = soma.nextInt();
+        int X = (a + b);
+        System.out.println("X = " +X);
+ 
+    }
+ 
+}
