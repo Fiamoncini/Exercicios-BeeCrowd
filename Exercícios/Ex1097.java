b499a4170630b15ed9f51a7800dae4d1364538a8
Fiamoncini <93059776+Fiamoncini@users.noreply.github.com>
2024-07-18 16:58:16 -0300

    Create Ex1097.java

---
+++ Exercícios/Ex1097.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+public class Main {
+ 
+    public static void main(String[] args) {
+        int i = 1;
+        int j = 7;
+        while (true) {
+            System.out.println("I="+i + " J="+j);
+            j -= 1;
+            System.out.println("I="+i + " J="+j);
+            j -= 1;
+            System.out.println("I="+i + " J="+j);
+            j += 4;
+            i += 2;
+            if (i == 11) {
+                break;
+            }
+            }
+        }
+ 
+}
