6cb34a25cf9615464ffa546c04383a2f1807da7d
Fiamoncini <93059776+Fiamoncini@users.noreply.github.com>
2024-07-18 16:27:44 -0300

    Create Ex1012.java

---
+++ Exercícios/Ex1012.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double A, B, C;
+        A = scanner.nextDouble();
+        B = scanner.nextDouble();
+        C = scanner.nextDouble();
+        double areaTriangulo = (A * C) / 2;
+        double pi = 3.14159;
+        double areaCirculo = pi * Math.pow(C, 2);
+        double areaTrapezio = ((A + B) * C) / 2;
+        double areaQuadrado = Math.pow(B, 2);
+        double areaRetangulo = A * B;
+        System.out.printf("TRIANGULO: %.3f%n", areaTriangulo);
+        System.out.printf("CIRCULO: %.3f%n", areaCirculo);
+        System.out.printf("TRAPEZIO: %.3f%n", areaTrapezio);
+        System.out.printf("QUADRADO: %.3f%n", areaQuadrado);
+        System.out.printf("RETANGULO: %.3f%n", areaRetangulo);
+
+        scanner.close();
+    }
+}
