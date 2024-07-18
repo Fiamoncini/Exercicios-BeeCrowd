8ad9a6a2d7fde34593ab6c8706fae8639439bc8a
Fiamoncini <93059776+Fiamoncini@users.noreply.github.com>
2024-07-18 16:58:39 -0300

    Create Ex1113.java

---
+++ Exercícios/Ex1113.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        while (true) {
+            int num = s.nextInt();
+            int num2 = s.nextInt();
+            if (num > num2) {
+                System.out.println("Decrescente");
+            }
+            if (num2 > num) {
+                System.out.println("Crescente");
+            }
+            if (num == num2) {
+                break;
+            }
+        }
+    }
+}
