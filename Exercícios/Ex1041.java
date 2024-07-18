04730ca7fbb7c0d9bab70e9d8a797439eec90871
Fiamoncini <93059776+Fiamoncini@users.noreply.github.com>
2024-07-18 16:41:44 -0300

    Create Ex1041.java

---
+++ Exercícios/Ex1041.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner s = new Scanner(System.in);
+        float x = s.nextFloat();
+        float y = s.nextFloat();
+        if (y > 0 && x < 0) {
+            System.out.println("Q2");
+        }
+        if (y > 0 && x > 0) {
+            System.out.println("Q1");
+        }
+        if (y < 0 && x < 0) {
+            System.out.println("Q3");
+        }
+        if (y < 0 && x > 0) {
+            System.out.println("Q4");
+        }
+        if (y == 0 && x == 0)
+        System.out.println("Origem");
+        s.close();
+        if (y == 0 && x != 0) {
+            System.out.println("Eixo X");
+        }
+        if (x == 0 && y != 0) {
+            System.out.println("Eixo Y");
+        }
+    }
+}
