deabc13f4fdbc1c88e04cf5a8a8c66a0f54c7824
Fiamoncini <93059776+Fiamoncini@users.noreply.github.com>
2024-07-18 16:21:26 -0300

    Create Ex1004.java

---
+++ Exercícios/Ex1004.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner soma = new Scanner(System.in);
+        int num1 = soma.nextInt();
+        int num2 = soma.nextInt();
+        int somar = num1 * num2;
+        System.out.println("PROD = "+somar);
+        soma.close();
+    }
+ 
+}
