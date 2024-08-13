ef809e003a6b4b5012533072b565fe7532d20457
Pedro Alegria Fiamoncini <93059776+Fiamoncini@users.noreply.github.com>
2024-08-13 09:39:44 -0300

    Create Ex2581.java

---
+++ Exercícios/Ex2581.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ex2581 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        String pergunta = "";
+        for(int i = 0; i < num; i++){
+            pergunta = s.next();
+            System.out.println("I am Toorg!");
+        }
+    }
+}
